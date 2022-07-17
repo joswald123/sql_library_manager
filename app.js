bd13e9ab27ee0614360ecdeca8750b8533b60a24
joswald123 <julianaandreaoswald@gmail.com>
2022-07-17 13:12:00 -0700

    update error 404 & global error app.js

--- app.js
+++ app.js
@@ -44,18 +44,29 @@ const sequelize = new Sequelize({
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
+  const err = new Error("Page not found!")
   next(createError(404));
+  err.status = 404;
+  res.render('page-not-found', { err })
+  
 });
 
-// error handler
+// error Global handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  // Checking if err is 404
+  if(err.status === 404 ) {
+    err.message = err.message || "Page not Found!"
+    res.render('page-not-found', { err })
+  } else {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('errors');
+  }
+  
 });
 
 module.exports = app;
